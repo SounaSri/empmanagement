5801dffaa08de99f5ed0362d033d4181a23fd1a1
K.SaiSounaSri <sounasrik@infinite.com>
2023-01-20 14:37:30 +0530

    done

---
+++ employeemanagement-system/src/test/java/com/project/springboot/employeemanagementsystem/EmployeemanagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springboot.employeemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
